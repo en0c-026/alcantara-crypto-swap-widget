57717ca60560a7796deda2c7c35f425f59af0c0e
Samsung <bitup.games@gmail.com>
2023-03-26 19:45:13 +0200

    fix: action button behavior

--- components/ActionButton.tsx
+++ components/ActionButton.tsx
@@ -4,27 +4,29 @@ import { Connect } from "./Connect"
 
 interface Props {
   onClick: () => void;
-  disabled?: boolean;
+  needMoreAllownce: boolean;
+  insufficientBalance: boolean;
+  disabled: boolean;
 }
 
-export default function ActionButton({ disabled, onClick }: Props) {
+export default function ActionButton({
+  onClick,
+  needMoreAllownce,
+  insufficientBalance,
+  disabled
+}: Props) {
   const { isConnected } = useAccount()
 
   if (!isConnected) {
-    return <Connect className="bg-blue-600 hover:bg-blue-500 delay-100 text-[1.1rem] font-semibold text-slate-50 w-full rounded-xl py-1 text-xl" />
+    return <Connect className="bg-blue-600 w-full rounded-xl py-2 text-lg text-slate-50 disabled:text-slate-200 hover:bg-blue-500 delay-100 hover:cursor-pointer disabled:bg-blue-400 hover:disabled:bg-blue-400 hover:disabled:cursor-default" />
   } else {
     return (
       <button
-        className="bg-blue-600 w-full rounded-xl py-2 text-xl text-slate-50 disabled:text-slate-200 hover:bg-blue-500 delay-100 hover:cursor-pointer disabled:bg-blue-300 hover:disabled:bg-blue-300 hover:disabled:cursor-default"
+        className="bg-blue-600 w-full rounded-xl h-11 py-2 text-lg text-slate-50 disabled:text-slate-200 hover:bg-blue-500 delay-100 hover:cursor-pointer disabled:bg-blue-400 hover:disabled:bg-blue-400 hover:disabled:cursor-default"
         onClick={onClick}
-        disabled={disabled}>
-        {
-          false ? (
-            <p className="text-[1.12rem] font-medium">Insufficient Balance</p>
-          ) : (
-            <p className="text-[1.12rem] font-medium ">Swap</p>
-          )
-        }
+        disabled={disabled}
+      >
+        {insufficientBalance ? <p>Insufficient Balance</p> : needMoreAllownce ? <p>Approve</p> : <p>Swap</p>}
       </button>
     )
   }
