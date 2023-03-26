828cb675739349e90bb50ec6aedadb46143ac1c8
Samsung <bitup.games@gmail.com>
2023-03-26 19:48:33 +0200

    update: getTokenAllowance function

--- contexts/AlchemyService.tsx
+++ contexts/AlchemyService.tsx
@@ -6,13 +6,13 @@ import { BigNumber } from "ethers";
 interface AlchemyServiceInterface {
   getTokenAllowance: (
     id: number,
+    amount: string,
     params: {
       token: string,
       owner: string,
       spender: string
-    },
-    amount: number
-  ) => Promise<any>;
+    }
+  ) => Promise<boolean>;
 }
 
 export const AlchemyServiceContext = createContext<AlchemyServiceInterface | undefined>(undefined);
@@ -21,37 +21,44 @@ interface Props {
   children: ReactNode;
 }
 const AlchemyServiceProvider = ({ network, children }: Props) => {
+  console.log(network)
   const service = useRef(createAlchemyClient(network.toLocaleLowerCase()));
 
   async function getTokenAllowance(
     id: number,
+    amount: string,
     params: {
       token: string,
       owner: string,
       spender: string
-    },
-    amount: number
-  ): Promise<any> {
+    }
+  ): Promise<boolean> {
     const { owner, spender, token } = params;
 
-    const response = await service.current.post<any>({
-      url: '',
-      method: 'POST',
-      headers: { accept: 'application/json', 'content-type': 'application/json' },
-      data: {
-        id,
-        jsonrpc: '2.0',
-        method: 'alchemy_getTokenAllowance',
-        params: [
-          {
-            contract: token,
-            owner,
-            spender,
-          }
-        ]
-      }
-    })
-    return BigNumber.from(response.result).gte(amount)
+    try {
+      const response = await service.current.post<any>({
+        url: '',
+        method: 'POST',
+        headers: { accept: 'application/json', 'content-type': 'application/json' },
+        data: {
+          id,
+          jsonrpc: '2.0',
+          method: 'alchemy_getTokenAllowance',
+          params: [
+            {
+              contract: token,
+              owner,
+              spender,
+            }
+          ]
+        }
+      })
+      return BigNumber.from(response.result).lte(amount)
+
+    } catch (error) {
+      console.log('error on getTokenAllowance', error)
+      return false
+    }
 
   }
 
